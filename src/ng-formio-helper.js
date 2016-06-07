b17fd4949a1a4f94c42b368447f90dd27ba89a77
Randall Knutson <randall@form.io>
2016-06-07 16:17:58 -0500

    Don't assume admins always have access. Use ng-if="isAdmin" instead.

--- src/ng-formio-helper.js
+++ src/ng-formio-helper.js
@@ -814,11 +814,6 @@ angular.module('ngFormioHelper', ['formio', 'ngFormioGrid', 'ui.router'])
                     return false;
                   }
 
-                  // If the user is an admin, then they always have access.
-                  if ($rootScope.isAdmin) {
-                    return true;
-                  }
-
                   var hasAccess = false;
                   permissions.forEach(function(permission) {
                     // Check for anonymous users. Must set anonRole.
@@ -830,6 +825,7 @@ angular.module('ngFormioHelper', ['formio', 'ngFormioGrid', 'ui.router'])
                     else {
                       // Check the user's roles for access.
                       $rootScope.user.roles.forEach(function(role) {
+                        console.log(role);
                         if (formAccess[form][permission].indexOf(role) !== -1) {
                           hasAccess = true;
                         }
