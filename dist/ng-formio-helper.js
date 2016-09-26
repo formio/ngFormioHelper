ac7b9c274777df3fb5a20bf1c326fdf02f49cb54
Travis Tidwell <travis@form.io>
2016-09-26 08:53:00 -0500

    Adding resource fields component to helper.

--- dist/ng-formio-helper.js
+++ dist/ng-formio-helper.js
@@ -2,6 +2,133 @@
 "use strict";
 
 angular.module('ngFormioHelper', ['formio', 'ngFormioGrid', 'ui.router'])
+  .config(['formioComponentsProvider', function(formioComponentsProvider) {
+    formioComponentsProvider.register('resourcefields', {
+      title: 'Resource Fields',
+      template: 'formio/components/resourcefields.html',
+      controller: [
+        '$scope',
+        '$rootScope',
+        '$http',
+        'FormioUtils',
+        'AppConfig',
+        function (
+            $scope,
+            $rootScope,
+            $http,
+            FormioUtils,
+            AppConfig
+        ) {
+          var settings = $scope.component;
+          var resourceExclude = '';
+          $scope.resourceComponents = [];
+          if ($rootScope.currentForm && $rootScope.currentForm._id) {
+            resourceExclude = '&_id__ne=' + $rootScope.currentForm._id;
+          }
+          $scope.resourceSelect = {
+            type: 'select',
+            input: true,
+            label: settings.title ? settings.title : 'Select a resource',
+            key: 'resource',
+            placeholder: settings.placeholder || '',
+            dataSrc: 'url',
+            data: {url: settings.basePath + '?type=resource' + resourceExclude},
+            valueProperty: '_id',
+            defaultValue: $scope.data.resource,
+            template: '<span>{{ item.title }}</span>',
+            multiple: false,
+            protected: false,
+            unique: false,
+            persistent: true,
+            validate: {
+              required: settings.hasOwnProperty('required') ? settings.required : true
+            }
+          };
+
+          $scope.propertyField = {
+            label: 'Resource Property',
+            key: 'property',
+            inputType: 'text',
+            input: true,
+            placeholder: 'Assign this resource to the following property',
+            prefix: '',
+            suffix: '',
+            type: 'textfield',
+            defaultValue: $scope.data.property,
+            multiple: false
+          };
+
+          // Keep track of the available forms on the provided form.
+          var formFields = [];
+
+          // Fetch the form information.
+          $http.get(AppConfig.apiUrl + settings.basePath + '/' + settings.form).then(function(result) {
+            FormioUtils.eachComponent(result.data.components, function(component) {
+              if (component.type !== 'button') {
+                formFields.push({
+                  value: component.key,
+                  label: component.label
+                });
+              }
+            });
+          });
+
+          // Watch the selection of a new resource and set the resource field information.
+          $scope.$watch('data.resource', function(data) {
+            if (!data) { return; }
+            $scope.data.fields = $scope.data.fields || {};
+            if (data !== $scope.data.resource) {
+              $scope.data.resource = data;
+            }
+            $scope.resourceComponents = [];
+            $http.get(AppConfig.apiUrl + settings.basePath + '/' + data).then(function(results) {
+              FormioUtils.eachComponent(results.data.components, function(component) {
+                if (component.type !== 'button') {
+                  $scope.resourceComponents.push({
+                    type: 'select',
+                    input: true,
+                    label: component.label,
+                    key: component.key,
+                    dataSrc: 'values',
+                    defaultValue: $scope.data.fields[component.key],
+                    data: { values: formFields },
+                    validate: {
+                      required: component.validate ? (component.validate.required) : false
+                    }
+                  });
+                }
+              });
+            });
+          });
+        }
+      ],
+      settings: {
+        input: true,
+        tableView: false,
+        builder: false,
+        inputType: 'text',
+        inputMask: '',
+        label: '',
+        key: 'textField',
+        placeholder: '',
+        prefix: '',
+        suffix: '',
+        multiple: false,
+        defaultValue: '',
+        protected: false,
+        unique: false,
+        persistent: true,
+        validate: {
+          required: false,
+          minLength: '',
+          maxLength: '',
+          pattern: '',
+          custom: '',
+          customPrivate: false
+        }
+      }
+    });
+  }])
   .filter('capitalize', [function () {
     return _.capitalize;
   }])
@@ -1116,9 +1243,13 @@ angular.module('ngFormioHelper', ['formio', 'ngFormioGrid', 'ui.router'])
     '$templateCache',
     '$rootScope',
     '$state',
-    function ($templateCache,
-              $rootScope,
-              $state) {
+    'FormioUtils',
+    function (
+        $templateCache,
+        $rootScope,
+        $state,
+        FormioUtils
+    ) {
       // Determine the active state.
       $rootScope.isActive = function (state) {
         return $state.current.name.indexOf(state) !== -1;
@@ -1208,6 +1339,10 @@ angular.module('ngFormioHelper', ['formio', 'ngFormioGrid', 'ui.router'])
       $templateCache.put('formio-helper/offline/button.html',
         "<div class=\"offline-button\">\n    <div ng-if=\"hasOfflineMode\">\n        <span class=\"navbar-text\" ng-click=\"offline.forceOffline(!offline.isForcedOffline())\" style=\"cursor:pointer;\">\n            <i class=\"glyphicon glyphicon-signal text-success\" ng-class=\"{ 'text-danger': (offline.isForcedOffline() || !offline.enabled), 'text-warning' : offline.offline }\"></i>\n        </span>\n        <span ng-if=\"offline.submissionQueueLength()\" ng-click=\"offline.dequeueSubmissions()\" class=\"navbar-text\" style=\"cursor:pointer;\">\n            <span class=\"badge\">{{ offline.submissionQueueLength() }} Queued</span> <i class=\"glyphicon glyphicon-refresh\" ng-class=\"{ 'glyphicon-spin': offline.dequeuing }\"></i>\n        </span>\n    </div>\n    <div ng-if=\"!hasOfflineMode\">\n        <span class=\"navbar-text\">\n            <a tabindex=\"0\" offline-popup role=\"button\" data-toggle=\"popover\" data-placement=\"bottom\" data-trigger=\"focus\" title=\"Offline Mode Disabled\" data-content=\"You must upgrade your project to Team Pro to enable offline mode support. Please contact support@form.io for more information.\"><i class=\"glyphicon glyphicon-signal text-danger\"></i></a>\n        </span>\n    </div>\n</div>"
       );
+
+      $templateCache.put('formio/components/resourcefields.html', FormioUtils.fieldWrap(
+          "<formio-component component=\"resourceSelect\" data=\"data\"></formio-component>\n<formio-component ng-if=\"data.resource\" component=\"propertyField\" data=\"data\"></formio-component>\n<fieldset ng-if=\"data.resource\">\n  <legend>Resource Fields</legend>\n  <div class=\"well\">Below are the fields within the selected resource. For each of these fields, select the corresponding field within this form that you wish to map to the selected Resource.</div>\n  <formio-component ng-repeat=\"resourceComponent in resourceComponents\" component=\"resourceComponent\" data=\"data.fields\"></formio-component>\n</fieldset>\n"
+      ));
     }
   ]);
 },{}]},{},[1]);
--- src/ng-formio-helper.js
+++ src/ng-formio-helper.js
@@ -1,5 +1,132 @@
 var fs = require('fs');
 angular.module('ngFormioHelper', ['formio', 'ngFormioGrid', 'ui.router'])
+  .config(['formioComponentsProvider', function(formioComponentsProvider) {
+    formioComponentsProvider.register('resourcefields', {
+      title: 'Resource Fields',
+      template: 'formio/components/resourcefields.html',
+      controller: [
+        '$scope',
+        '$rootScope',
+        '$http',
+        'FormioUtils',
+        'AppConfig',
+        function (
+            $scope,
+            $rootScope,
+            $http,
+            FormioUtils,
+            AppConfig
+        ) {
+          var settings = $scope.component;
+          var resourceExclude = '';
+          $scope.resourceComponents = [];
+          if ($rootScope.currentForm && $rootScope.currentForm._id) {
+            resourceExclude = '&_id__ne=' + $rootScope.currentForm._id;
+          }
+          $scope.resourceSelect = {
+            type: 'select',
+            input: true,
+            label: settings.title ? settings.title : 'Select a resource',
+            key: 'resource',
+            placeholder: settings.placeholder || '',
+            dataSrc: 'url',
+            data: {url: settings.basePath + '?type=resource' + resourceExclude},
+            valueProperty: '_id',
+            defaultValue: $scope.data.resource,
+            template: '<span>{{ item.title }}</span>',
+            multiple: false,
+            protected: false,
+            unique: false,
+            persistent: true,
+            validate: {
+              required: settings.hasOwnProperty('required') ? settings.required : true
+            }
+          };
+
+          $scope.propertyField = {
+            label: 'Resource Property',
+            key: 'property',
+            inputType: 'text',
+            input: true,
+            placeholder: 'Assign this resource to the following property',
+            prefix: '',
+            suffix: '',
+            type: 'textfield',
+            defaultValue: $scope.data.property,
+            multiple: false
+          };
+
+          // Keep track of the available forms on the provided form.
+          var formFields = [];
+
+          // Fetch the form information.
+          $http.get(AppConfig.apiUrl + settings.basePath + '/' + settings.form).then(function(result) {
+            FormioUtils.eachComponent(result.data.components, function(component) {
+              if (component.type !== 'button') {
+                formFields.push({
+                  value: component.key,
+                  label: component.label
+                });
+              }
+            });
+          });
+
+          // Watch the selection of a new resource and set the resource field information.
+          $scope.$watch('data.resource', function(data) {
+            if (!data) { return; }
+            $scope.data.fields = $scope.data.fields || {};
+            if (data !== $scope.data.resource) {
+              $scope.data.resource = data;
+            }
+            $scope.resourceComponents = [];
+            $http.get(AppConfig.apiUrl + settings.basePath + '/' + data).then(function(results) {
+              FormioUtils.eachComponent(results.data.components, function(component) {
+                if (component.type !== 'button') {
+                  $scope.resourceComponents.push({
+                    type: 'select',
+                    input: true,
+                    label: component.label,
+                    key: component.key,
+                    dataSrc: 'values',
+                    defaultValue: $scope.data.fields[component.key],
+                    data: { values: formFields },
+                    validate: {
+                      required: component.validate ? (component.validate.required) : false
+                    }
+                  });
+                }
+              });
+            });
+          });
+        }
+      ],
+      settings: {
+        input: true,
+        tableView: false,
+        builder: false,
+        inputType: 'text',
+        inputMask: '',
+        label: '',
+        key: 'textField',
+        placeholder: '',
+        prefix: '',
+        suffix: '',
+        multiple: false,
+        defaultValue: '',
+        protected: false,
+        unique: false,
+        persistent: true,
+        validate: {
+          required: false,
+          minLength: '',
+          maxLength: '',
+          pattern: '',
+          custom: '',
+          customPrivate: false
+        }
+      }
+    });
+  }])
   .filter('capitalize', [function () {
     return _.capitalize;
   }])
@@ -1114,9 +1241,13 @@ angular.module('ngFormioHelper', ['formio', 'ngFormioGrid', 'ui.router'])
     '$templateCache',
     '$rootScope',
     '$state',
-    function ($templateCache,
-              $rootScope,
-              $state) {
+    'FormioUtils',
+    function (
+        $templateCache,
+        $rootScope,
+        $state,
+        FormioUtils
+    ) {
       // Determine the active state.
       $rootScope.isActive = function (state) {
         return $state.current.name.indexOf(state) !== -1;
@@ -1206,5 +1337,9 @@ angular.module('ngFormioHelper', ['formio', 'ngFormioGrid', 'ui.router'])
       $templateCache.put('formio-helper/offline/button.html',
         fs.readFileSync(__dirname + '/templates/offline/button.html', 'utf8')
       );
+
+      $templateCache.put('formio/components/resourcefields.html', FormioUtils.fieldWrap(
+          fs.readFileSync(__dirname + '/templates/components/resourcefields.html', 'utf8')
+      ));
     }
   ]);
